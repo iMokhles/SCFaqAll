dd04df82d6ff378802f0ff7ba0d760e86141d4a1
iMokhles <mokhleshussien@aol.com>
2017-09-03 16:28:27 +0200

    add files

---
+++ SCFaqAll/SCFaqAll.h
@@ -0,0 +1,13 @@
+//
+//  SCFaqAll.h
+//  SCFaqAll
+//
+//  Created by iMokhles on 03/09/2017.
+//  Copyright © 2017 iMokhles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCFaqAll : NSObject
+
+@end
---
+++ SCFaqAll/SCFaqAll.m
@@ -0,0 +1,13 @@
+//
+//  SCFaqAll.m
+//  SCFaqAll
+//
+//  Created by iMokhles on 03/09/2017.
+//  Copyright © 2017 iMokhles. All rights reserved.
+//
+
+#import "SCFaqAll.h"
+
+@implementation SCFaqAll
+
+@end
